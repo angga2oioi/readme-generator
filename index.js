42aca193a1fc7ab4ae5403e7a8a27f35ea7d2fdd
Rizka Sanggar Darmawan <angga2oioi@gmail.com>
2025-03-05 13:27:02 +0700

    now can use different model

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const start = async () => {
 
     try {
         const directory = await jaci.string("directory : ", { required: true })
-        const strConfig = await jaci.string("Open AI Config : ", { required: true })
-        let connection = JSON.parse(strConfig)
+        const strConnection = await jaci.string("Open AI Connection : ", { required: true })
+        const model = await jaci.string("AI Model : ", { required: true })
+        let connection = JSON.parse(strConnection)
 
-        let readme = await generateReadme(directory, connection)
+        let readme = await generateReadme(directory, { connection, model })
         await writeFile(`${directory}/readme.md`, readme)
 
         console.info(`${directory}/readme.md created`)
